ddd92c5e3fca284fec4e6a805156573ba9164387
soraminn-zzz <soracoco10031219@gmail.com>
2025-10-22 14:21:43 +0900

    feat: データ取得の関数ファイルの追加

---
+++ src/services/data.js
@@ -0,0 +1,39 @@
+import { DB } from '../database/db.js'
+
+TEST_USER = "test";
+
+database = DB(TEST_USER);
+
+// Get関数
+
+//引数：Key ("HP", "MP", EN, MY)
+function getData(key) {
+    if (key){
+        data = database.getStatusOne(key);
+    } else {
+        data = database.getStatusAll();
+    }
+    return data;
+}
+
+// Update関数
+
+function setData(key, val) {
+    data = database.getStatusOne("parameter");
+
+    data[key] = val;
+
+    database.updateStatusOne("parameter", data);
+
+    return data;
+}
+
+function modifyData(key, val) {
+    data = database.getStatusOne("parameter");
+
+    data[key] += val;
+
+    database.updateStatusOne("parameter", data);
+
+    return data;
+}
