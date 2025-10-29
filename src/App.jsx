5ca10787a7fc3b7dbf1ca2c02b3c58180e0b7292
pansyu <gutti1225yokono@gmail.com>
2025-10-29 13:32:02 +0900

    feat,ファイル分け・ボトムナビゲーション

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ResourceCounter from './components/ResourceCounter'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ResourceCounter from "./components/Home/ResourceCounter";
+import BottomNavigation from "./components/common/BottomNavigation";
+
+function Tweet() {
+  return <h2>Tweet Page</h2>;
+}
+function Game1() {
+  return <h2>Game1 Page</h2>;
+}
+function Game2() {
+  return <h2>Game2 Page</h2>;
+}
+function Profile() {
+  return <h2>Profile Page</h2>;
+}
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div style={{ paddingBottom: "60px" }}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>自己管理</h1>
+                <ResourceCounter />
+                <p className="read-the-docs">
+                  Click on the Vite and React logos to learn more
+                </p>
+              </>
+            }
+          />
+          <Route path="/tweet" element={<Tweet />} />
+          <Route path="/game1" element={<Game1 />} />
+          <Route path="/game2" element={<Game2 />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <BottomNavigation />
       </div>
-      <h1>Vite + React</h1>
-      
-      {/* リソース管理コンポーネント */}
-      <ResourceCounter />
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
