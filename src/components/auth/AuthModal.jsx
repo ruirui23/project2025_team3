d89776dd269bcf9f28c1c1385fb103e63ad7306b
soraminn-zzz <soracoco10031219@gmail.com>
2026-01-14 14:28:58 +0900

    feat login system

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LoginModal from "./LoginModal";
 import SignupModal from "./SignupModal";
+import { IndexDB } from "../../database/index_db.js";
 
 export default function AuthModals({
   loginOpen,
@@ -12,16 +13,55 @@ export default function AuthModals({
   // 実装が進んだら Context/Store に移せる
   const [authSubmitting, setAuthSubmitting] = useState(false);
 
+  const API_URL = "http://localhost:5173/auth"
+
   const loginApi = async ({ userName, password, remember }) => {
-    // TODO: API接続に置き換え
-    await new Promise((r) => setTimeout(r, 400));
-    return { ok: true };
+    try {
+      const response = await fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: userName, password: password}),
+      });
+
+      if(!response.ok) {
+        throw new Error("ログインに失敗しました");
+      }
+      const data = await response.json();
+      //成功したらローカルに一時保存
+      const db = new IndexDB("GameDatabase", data.userId);
+
+      await db.syncUserFromServer(data.userId, data.userName, data.token);
+
+      return { ok: true };
+    } catch (error) {
+      console.error(error);
+      return { ok: false, error: error.message };
+    }
   };
 
   const signupApi = async ({ userName, password }) => {
-    // TODO: API接続に置き換え
-    await new Promise((r) => setTimeout(r, 550));
-    return { ok: true };
+    try {
+      const response = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: userName, password: password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("登録に失敗しました");
+      }
+
+      const data = await response.json();
+      
+      // サインアップ直後に自動ログイン状態にするなら、ここでも保存処理を行う
+      const db = new IndexDB("GameDatabase", data.userId);
+      await db.syncUserFromServer(data.userId, data.userName, data.token);
+
+      return { ok: true };
+    } catch (error) {
+      console.error(error);
+      return { ok: false, error: error.message };
+    }
   };
 
   return (
